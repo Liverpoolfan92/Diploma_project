b75ed7e7b2fe2bdf45a3df029e2c7fce4f237012
Liverpoolfan92 <85640417+Liverpoolfan92@users.noreply.github.com>
2023-01-23 17:39:47 +0200

    updated version

--- test.go
+++ test.go
@@ -2,65 +2,70 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"os/exec"
-	"regexp"
 	"strings"
 )
 
-func main() {
-	fmt.Println("Select a network interface:")
-	fmt.Println("1. Wired")
-	fmt.Println("2. Wireless")
-
-	var interfaceNum int
-	fmt.Scan(&interfaceNum)
-
-	var interfaceName string
-
-	if interfaceNum == 1 {
-		// wired interface
-		output, _ := exec.Command("ip", "link").CombinedOutput()
-		regex := regexp.MustCompile(`(?m)^[0-9]+: (.*?):.*?state UP`)
-		matches := regex.FindAllStringSubmatch(string(output), -1)
-		for _, match := range matches {
-			if strings.Contains(match[1], "eth0") {
-				interfaceName = match[1]
-				break
-			}
-		}
-	} else if interfaceNum == 2 {
-		// wireless interface
-		output, _ := exec.Command("ip", "link").CombinedOutput()
-		regex := regexp.MustCompile(`(?m)^[0-9]+: (.*?):.*?state UP`)
-		matches := regex.FindAllStringSubmatch(string(output), -1)
-		for _, match := range matches {
-			if strings.Contains(match[1], "wlan") {
-				interfaceName = match[1]
-				break
-			}
-		}
-	} else {
-		fmt.Println("Invalid selection. Only 1 or 2 is allowed.")
-		return
-	}
-
-	if interfaceName == "" {
-		fmt.Println("no such interface found")
-		return
-	}
-
-	fmt.Print("Enter IP address to ping: ")
-
-	var ipAddress string
-	fmt.Scan(&ipAddress)
-
-	pingCmd := exec.Command("ping", "-I", interfaceName, ipAddress)
-
-	output, err := pingCmd.CombinedOutput()
+func ping(host string, interfaceName string) {
+	// run the 'ping' command and capture the output
+	out, err := exec.Command("ping", "-I", interfaceName, "-c", "5", host).Output()
 	if err != nil {
-		fmt.Printf("Command failed: %s\n", err)
+		fmt.Printf("Error: %s\n", err.Error())
 		return
 	}
 
-	fmt.Println(strings.TrimSpace(string(output)))
+	// parse the output
+	output := strings.TrimSpace(string(out))
+	fmt.Printf("%s\n", output)
+}
+
+func arp(host string, interfaceName string) {
+	// run the 'arp' command and capture the output
+	out, err := exec.Command("arp", "-i", interfaceName, "-n", host).Output()
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+		return
+	}
+
+	// parse the output
+	output := strings.TrimSpace(string(out))
+	fmt.Printf("%s\n", output)
+}
+
+func main() {
+
+	interfaces, _ := net.Interfaces()
+	var interfaceNames []string
+	for _, i := range interfaces {
+		interfaceNames = append(interfaceNames, i.Name)
+	}
+	var in string
+	var inter string
+	fmt.Println("Choose an interface:", interfaceNames)
+	fmt.Scanf("%s", &in)
+	switch in {
+	case "lo":
+		inter = interfaceNames[0]
+	case "eth0":
+		inter = interfaceNames[1]
+	case "eth1":
+		inter = interfaceNames[2]
+	case "docker0":
+		inter = interfaceNames[3]
+	}
+	var input1 string
+	var input2 string
+	fmt.Println("Enter ping or arp + ip address")
+	fmt.Scanf("%s", &input1)
+	fmt.Scanf("%s", &input2)
+
+	switch input1 {
+	case "ping":
+		ping(input2, inter)
+	case "arp":
+		arp(input2, inter)
+	default:
+		fmt.Println("Invalid input.")
+	}
 }
