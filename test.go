1f6b9dfe6b2d69b0288ce74eba9eef8e46c827f1
Liverpoolfan92 <85640417+Liverpoolfan92@users.noreply.github.com>
2023-01-23 17:51:57 +0200

    automatic search of interfaces

--- test.go
+++ test.go
@@ -34,37 +34,67 @@ func arp(host string, interfaceName string) {
 }
 
 func main() {
-
 	interfaces, _ := net.Interfaces()
 	var interfaceNames []string
 	for _, i := range interfaces {
 		interfaceNames = append(interfaceNames, i.Name)
 	}
-	var in string
-	var inter string
-	fmt.Println("Choose an interface:", interfaceNames)
-	fmt.Scanf("%s", &in)
-	switch in {
-	case "lo":
-		inter = interfaceNames[0]
-	case "eth0":
-		inter = interfaceNames[1]
-	case "eth1":
-		inter = interfaceNames[2]
-	case "docker0":
-		inter = interfaceNames[3]
+
+	fmt.Println("Select a network interface:")
+	fmt.Println("1. Wired")
+	fmt.Println("2. Wireless")
+
+	var interfaceNum int
+	fmt.Scan(&interfaceNum)
+
+	var interfaceName string
+
+	if interfaceNum == 1 {
+		// wired interface
+		for _, i := range interfaces {
+			if i.Flags&net.FlagUp != 0 &&
+				i.Flags&net.FlagLoopback == 0 &&
+				!strings.HasPrefix(i.Name, "docker") &&
+				!strings.HasPrefix(i.Name, "wlan") {
+				interfaceName = i.Name
+				break
+			}
+		}
+	} else if interfaceNum == 2 {
+		// wireless interface
+		for _, i := range interfaces {
+			if i.Flags&net.FlagUp != 0 &&
+				i.Flags&net.FlagLoopback == 0 &&
+				!strings.HasPrefix(i.Name, "docker") &&
+				strings.HasPrefix(i.Name, "wlan") {
+				interfaceName = i.Name
+				break
+			}
+		}
+	} else {
+		fmt.Println("Invalid selection. Only 1 or 2 is allowed.")
+		return
 	}
+
+	if interfaceName == "" {
+		fmt.Println("No such interface found.")
+		return
+	}
+
+	fmt.Print("Enter IP address to ping or arp: ")
+
+	var ipAddress string
+	fmt.Scan(&ipAddress)
+
 	var input1 string
-	var input2 string
-	fmt.Println("Enter ping or arp + ip address")
+	fmt.Println("Enter ping or arp")
 	fmt.Scanf("%s", &input1)
-	fmt.Scanf("%s", &input2)
 
 	switch input1 {
 	case "ping":
-		ping(input2, inter)
+		ping(ipAddress, interfaceName)
 	case "arp":
-		arp(input2, inter)
+		arp(ipAddress, interfaceName)
 	default:
 		fmt.Println("Invalid input.")
 	}
