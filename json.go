c3bc05d1b9ea2a004b8b39e2b3c49734d8da5d5c
Liverpoolfan92 <85640417+Liverpoolfan92@users.noreply.github.com>
2023-02-08 00:46:11 +0200

    Update json.go

--- json.go
+++ json.go
@@ -46,7 +46,7 @@ func main() {
 	// Send the interface names to port 8484
 	conn, err := net.Dial("tcp", hostIP+":8484")
 	if err != nil {
-		fmt.Printf("Error connecting to port 8484: %s\n"+"ip:"+hostIP+"\n", err.Error())
+		fmt.Printf("Error connecting to port 8484 \n"+"ip:"+hostIP+"\n %s", err.Error())
 		return
 	}
 	defer conn.Close()
