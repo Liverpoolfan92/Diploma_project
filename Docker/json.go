40bc383c01eb36ef4ffa82927dd67fdb18a4e745
Liverpoolfan92 <85640417+Liverpoolfan92@users.noreply.github.com>
2023-02-08 14:31:24 +0200

    Update json.go

--- Docker/json.go
+++ Docker/json.go
@@ -32,7 +32,7 @@ func main() {
 	}
 
 	// Send the interface names to port 8484
-	conn, err := net.Dial("tcp", " docker0:8484")
+	conn, err := net.Dial("tcp", " :8484")
 	if err != nil {
 		fmt.Printf("Error connecting to port 8484: %s\n", err.Error())
 		return
