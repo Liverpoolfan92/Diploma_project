f5074af5e49652537d6a363d6846b8cf049ee567
Liverpoolfan92 <85640417+Liverpoolfan92@users.noreply.github.com>
2023-02-10 00:02:30 +0200

    config the communication from one side

---
+++ test_gui/ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Diagnostics;
+
+namespace DockerRunScript
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hostIP = GetLocalIPAddress();
+            Console.WriteLine(hostIP);
+            string dockerCommand = "docker run -dit --rm --network host --name PacketGenerator --ip="+ "192.168.65.99 --env VAR1=" + hostIP + " test_1 bash";
+
+            ExecuteDockerCommand(dockerCommand);
+        }
+
+        static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            return null;
+        }
+
+        static void ExecuteDockerCommand(string command)
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C " + command;
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+    }
+}
